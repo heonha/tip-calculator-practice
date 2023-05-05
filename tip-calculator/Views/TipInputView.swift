7f6000277f00f9a064d9333e0ecc748a51ea01c8
HeonJin Ha <weizen511@gmail.com>
2023-05-05 21:35:31 +0900

    style: TipInputView 스타일 작성

--- tip-calculator/Views/TipInputView.swift
+++ tip-calculator/Views/TipInputView.swift
@@ -6,10 +6,42 @@
 //
 
 import UIKit
+import SnapKit
 
 class TipInputView: UIView {
 
-    
+    let headerView = HeaderView(topTitle: "Choose", bottomTitle: "your tip")
+
+    private lazy var tenPercentButton: UIButton = {
+        return buildTipButton(tip: .tenPercent)
+    }()
+
+    private lazy var fiftyPercentButton: UIButton = {
+        return buildTipButton(tip: .fiftenPercent)
+    }()
+
+    private lazy var twentyPercentButton: UIButton = {
+        return buildTipButton(tip: .twentyPercent)
+    }()
+
+    private lazy var customTipButton: UIButton = {
+        return buildTipButton(tip: .custom(value: 0))
+    }()
+
+    private lazy var buttonContainerView: UIStackView = {
+        let hStackView = UIStackView(arrangedSubviews: [tenPercentButton, fiftyPercentButton, twentyPercentButton])
+        hStackView.axis = .horizontal
+        hStackView.distribution = .fillEqually
+        hStackView.spacing = 8
+
+        let vStackView = UIStackView(arrangedSubviews: [hStackView, customTipButton])
+        vStackView.axis = .vertical
+        vStackView.distribution = .fillEqually
+        vStackView.spacing = 8
+        
+
+        return vStackView
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -21,10 +53,50 @@ class TipInputView: UIView {
     }
 
     private func layout() {
-        self.backgroundColor = .blue
         self.snp.makeConstraints { make in
-            make.height.equalTo(56)
+            make.height.equalTo(56 + 56 + 8)
         }
+
+        [headerView, buttonContainerView].forEach { addSubview($0) }
+
+        headerView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.centerY.equalTo(tenPercentButton.snp.centerY)
+            make.width.equalTo(68)
+            make.trailing.equalTo(buttonContainerView.snp.leading).offset(-24)
+        }
+
+        buttonContainerView.snp.makeConstraints { make in
+            make.top.trailing.bottom.equalToSuperview()
+        }
+
+
+    }
+
+    func buildTipButton(tip: Tip) -> UIButton {
+
+        var attrString: NSMutableAttributedString
+        if tip.stringValue.contains("%") {
+            attrString = NSMutableAttributedString(string: tip.stringValue,
+                                                   attributes: [
+                                                    .font: AppFont.bold(ofSize: 20),
+                                                    .foregroundColor: UIColor.white
+                                                   ])
+            attrString.addAttributes([.font: AppFont.regular(ofSize: 14)], range: NSMakeRange(2, 1))
+        } else {
+            attrString = NSMutableAttributedString(string: "Custom tip",
+                                                   attributes: [
+                                                    .font: AppFont.bold(ofSize: 20),
+                                                    .foregroundColor: UIColor.white
+                                                   ])
+        }
+
+        let button = UIButton()
+        button.backgroundColor = AppColor.primary
+        button.setAttributedTitle(attrString, for: .normal)
+        button.addCornerRadius(radius: 8)
+
+        return button
     }
 
 }
